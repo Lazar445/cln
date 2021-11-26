76b6146b2b26c65270785280b42381df598c2769
Lazar445 <74473363+Lazar445@users.noreply.github.com>
2021-11-26 15:24:01 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,252 @@
+/**
+ * Code by Nicko - yt.com/nickoscriptz
+ * github.com/destroyednicko | t.me/spartey
+ * 
+ * Mirrored Cloner - 1.0
+ */
+
+const { Client } = require('discord.js-selfbot-v11'),
+    client = new Client();
+
+const { prompt } = require('enquirer')
+var colors = require('node-colors-sync')
+
+//const delay = async(ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+async function run() {
+
+    await logAscii()
+    process.title = '@nickoscriptz | Mirrored Cloner [Config]'
+
+    const config = await prompt([{
+            type: 'input',
+            name: 'token',
+            message: 'Insert your account token'
+        }, {
+            type: 'input',
+            name: 'original',
+            message: 'Insert the original server ID (to copy)'
+        },
+        {
+            type: 'input',
+            name: 'target',
+            message: 'Insert the target server ID'
+        }
+    ])
+    const { token, original, target } = config
+
+    client.on('ready', async() => {
+        logAscii()
+        const guilds = [await client.guilds.get(original), await client.guilds.get(target)]
+        guilds.forEach(g => {
+            if (!g) {
+                log('Unknown server, verify the ID!', 3)
+                process.exit(1)
+            }
+        })
+
+        let itens = {
+            text: guilds[0].channels.filter(c => c.type === 'text').sort((a, b) => a.calculatedPosition - b.calculatedPosition).map(c => c),
+            voice: guilds[0].channels.filter(c => c.type === 'voice').sort((a, b) => a.calculatedPosition - b.calculatedPosition).map(c => c),
+            category: guilds[0].channels.filter(c => c.type === 'category').sort((a, b) => a.calculatedPosition - b.calculatedPosition).map(c => c),
+            roles: guilds[0].roles.sort((a, b) => b.calculatedPosition - a.calculatedPosition).map(r => r)
+        }
+        process.title = `@nickoscriptz | Mirrored Cloner [Cloning ${guilds[0].name}]`;
+
+        log('Deleting all channels and roles of target guild...', 1)
+        await guilds[1].channels.forEach(c => c.delete().catch(() => {}))
+        await guilds[1].roles.map(r => r.delete().catch((() => {})))
+
+        await guilds[1].setIcon(guilds[0].iconURL)
+        await guilds[1].setName(`${guilds[0].name}*mirrored`)
+
+        for (let role of itens.roles) {
+            if (guilds[1].roles.get(role.id)) continue;
+
+            guilds[1].createRole({
+                name: role.name,
+                type: role.type,
+                color: role.color,
+                permissions: role.permissions,
+                managed: role.managed,
+                mentionable: role.mentionable,
+                position: role.position
+            }).then(r => log(`Created role: ${r.name}`, 1))
+        }
+
+        await guilds[0].emojis.forEach(e => {
+            if (guilds[1].emojis.get(e.id)) return;
+
+            guilds[1].createEmoji(e.url, e.name).then(c => log(`Created emoji: ${c}`, 1));
+        })
+
+        itens['category'].forEach(async(category) => {
+            if (guilds[1].channels.get(category.id)) return;
+
+            await guilds[1].createChannel(category.name, {
+                type: 'category',
+                permissionOverwrites: category.permissionOverwrites.map(v => {
+                    let target = guilds[0].roles.get(v.id);
+                    if (!target) return;
+                    return {
+                        id: guilds[1].roles.find(r => r.name == target.name) || guilds[1].id,
+                        allow: v.allow || 0,
+                        deny: v.deny || 0,
+                    };
+                }).filter(v => v),
+                position: category.position
+            }).then(c => {
+                log(`Created category: ${c.name}`, 1)
+            })
+        })
+
+        for (let channel of itens.text) {
+            if (guilds[1].channels.get(channel.id)) continue;
+
+            if (!channel.parent) {
+                if (channel.topic) await guilds[1].createChannel(channel.name, {
+                    type: 'text',
+                    permissionOverwrites: channel.permissionOverwrites.map(v => {
+                        let target = guilds[0].roles.get(v.id);
+                        if (!target) return;
+                        return {
+                            id: guilds[1].roles.find(r => r.name == target.name) || guilds[1].id,
+                            allow: v.allow || 0,
+                            deny: v.deny || 0,
+                        };
+                    }).filter(v => v),
+                    position: channel.position
+                }).then(c => c.setTopic(channel.topic))
+            } else {
+                let chn = await guilds[1].createChannel(channel.name, {
+                    type: 'text',
+                    permissionOverwrites: channel.permissionOverwrites.map(v => {
+                        let target = guilds[0].roles.get(v.id);
+                        if (!target) return;
+                        return {
+                            id: guilds[1].roles.find(r => r.name == target.name) || guilds[1].id,
+                            allow: v.allow || 0,
+                            deny: v.deny || 0,
+                        };
+                    }).filter(v => v),
+                    position: channel.position
+                })
+                if (channel.topic) chn.setTopic(channel.topic);
+
+                if (guilds[1].channels.find(c => c.name == channel.parent.name)) chn.setParent(guilds[1].channels.find(c => c.name == channel.parent.name).id);
+                else {
+                    var cat = await guilds[1].createChannel(channel.parent.name, {
+                        type: 'category',
+                        permissionOverwrites: channel.permissionOverwrites.map(v => {
+                            let target = guilds[0].roles.get(v.id);
+                            if (!target) return;
+                            return {
+                                id: guilds[1].roles.find(r => r.name == target.name) || guilds[1].id,
+                                allow: v.allow || 0,
+                                deny: v.deny || 0,
+                            };
+                        }).filter(v => v),
+                        position: channel.position
+                    });
+                    chn.setParent(cat);
+                }
+            }
+            log(`Created channel: ${channel.name}`, 1)
+        }
+
+        for (let channel of itens.voice) {
+            if (guilds[1].channels.get(channel.id)) continue;
+
+            if (!channel.parent) {
+                if (channel.topic) await guilds[1].createChannel(channel.name, {
+                    type: 'voice',
+                    permissionOverwrites: channel.permissionOverwrites.map(v => {
+                        let target = guilds[0].roles.get(v.id);
+                        if (!target) return;
+                        return {
+                            id: guilds[1].roles.find(r => r.name == target.name) || guilds[1].id,
+                            allow: v.allow || 0,
+                            deny: v.deny || 0,
+                        };
+                    }).filter(v => v),
+                    position: channel.position,
+                    userLimit: channel.userLimit
+                })
+            } else {
+                let chn = await guilds[1].createChannel(channel.name, {
+                    type: 'voice',
+                    permissionOverwrites: channel.permissionOverwrites.map(v => {
+                        let target = guilds[0].roles.get(v.id);
+                        if (!target) return;
+                        return {
+                            id: guilds[1].roles.find(r => r.name == target.name) || guilds[1].id,
+                            allow: v.allow || 0,
+                            deny: v.deny || 0,
+                        };
+                    }).filter(v => v),
+                    position: channel.position,
+                    userLimit: channel.userLimit
+                })
+
+                if (guilds[1].channels.find(c => c.name == channel.parent.name)) chn.setParent(guilds[1].channels.find(c => c.name == channel.parent.name).id);
+                else {
+                    var cat = await guilds[1].createChannel(channel.parent.name, {
+                        type: 'category',
+                        permissionOverwrites: channel.permissionOverwrites.map(v => {
+                            let target = guilds[0].roles.get(v.id);
+                            if (!target) return;
+                            return {
+                                id: guilds[1].roles.find(r => r.name == target.name) || guilds[1].id,
+                                allow: v.allow || 0,
+                                deny: v.deny || 0,
+                            };
+                        }).filter(v => v),
+                        position: channel.position,
+                    });
+                    chn.setParent(cat);
+                }
+            }
+            log(`Created channel: ${channel.name}`, 1)
+        }
+    })
+
+    client.login(`${token}`.replace(/"/g, ''))
+        .catch(() => {
+            logAscii()
+            log('Ops! Something is wrong, verify the token!', 3)
+        })
+}
+
+async function logAscii() {
+    console.clear()
+    console.log(`
+
+
+
+                888b     d888 d8b                                              888 
+                8888b   d8888 Y8P       youtube.com/nickoscriptz               888 
+                88888b.d88888                                                  888 
+                888Y88888P888 888 888d888 888d888 .d88b.  888d888 .d88b.   .d88888 
+                888 Y888P 888 888 888P"   888P"  d88""88b 888P"  d8P  Y8b d88" 888 
+                888  Y8P  888 888 888     888    888  888 888    88888888 888  888 
+                888   "   888 888 888     888    Y88..88P 888    Y8b.     Y88b 888 
+                888       888 888 888     888     "Y88P"  888     "Y8888   "Y88888 
+                        the ultimate discord cloner | open-source solution 
+`.brightMagenta)
+}
+
+async function log(message, type) {
+    switch (type) {
+        case 1:
+            await console.log(` [\u2713] ${message}`.brightGreen)
+            break;
+        case 2:
+            await console.log(` [\u26A0] ${message}`.yellow)
+            break;
+        case 3:
+            await console.log(` [\u274C] ${message}`.red)
+            break;
+    }
+}
+
+run()
